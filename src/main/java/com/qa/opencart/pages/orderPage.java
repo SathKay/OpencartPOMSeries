176a588b4e70e0a29c87c88c48e35b514add6d5a
SathKay <sathyakarunanithitce@gmail.com>
2023-06-03 19:15:01 +0530

    updated orderPage as per review comments

--- src/main/java/com/qa/opencart/pages/orderPage.java
+++ src/main/java/com/qa/opencart/pages/orderPage.java
@@ -1,9 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class orderPage {
+	
+	private static String orderId = "12345";
+	
+	By locator = By.name("order");
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("The order id is "+ orderId);
 
 	}
 
